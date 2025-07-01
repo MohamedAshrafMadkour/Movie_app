a4925dc2c99c05189934b6aadcc907ed7b590489
Mohamed Madkour <madkourmohamed88@gmail.com>
2025-07-01 15:26:09 +0300

    add images and create assets file

---
+++ lib/core/utils/assets.dart
@@ -0,0 +1,16 @@
+// ignore_for_file: prefer_single_quotes
+class Assets {
+  Assets._();
+
+  /// Assets for imagesArcher
+  /// assets/images/archer.svg
+  static const String imagesArcher = "assets/images/archer.svg";
+
+  /// Assets for imagesFrame 162756
+  /// assets/images/Frame 162756.svg
+  static const String imagesFrame = "assets/images/Frame 162756.svg";
+
+  /// Assets for imagesShowman
+  /// assets/images/showman.svg
+  static const String imagesShowman = "assets/images/showman.svg";
+}
