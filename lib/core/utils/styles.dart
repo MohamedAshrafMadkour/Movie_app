fed592d1bf871c6f81de0bf7ee91629f37478eb6
Mohamed Madkour <madkourmohamed88@gmail.com>
2025-07-01 15:10:51 +0300

    make a styles file

---
+++ lib/core/utils/styles.dart
@@ -0,0 +1,88 @@
+import 'package:flutter/material.dart';
+
+abstract class Styles {
+  static const TextStyle textBold30 = TextStyle(
+    color: Color(0xFFFEFEFE),
+    fontSize: 30,
+    fontFamily: 'Plus Jakarta Sans',
+    fontWeight: FontWeight.w700,
+  );
+  static const TextStyle textBold20 = TextStyle(
+    color: Color(0xFF02050F),
+    fontSize: 20,
+    fontFamily: 'Mulish',
+    fontWeight: FontWeight.w700,
+    height: 1.40,
+    letterSpacing: 0.10,
+  );
+  static const TextStyle textRegular14 = TextStyle(
+    color: Color(0xFF4C4E55),
+    fontSize: 14,
+    fontFamily: 'Mulish',
+    fontWeight: FontWeight.w400,
+    height: 1.57,
+    letterSpacing: 0.07,
+  );
+
+  static const TextStyle textSemiBold16 = TextStyle(
+    color: Color(0xFFFEFEFE),
+    fontSize: 16,
+    fontFamily: 'Mulish',
+    fontWeight: FontWeight.w600,
+    height: 1.50,
+    letterSpacing: 0.08,
+  );
+
+  static const TextStyle textRegular10 = TextStyle(
+    color: Color(0xFF34363E),
+    fontSize: 10,
+    fontFamily: 'Mulish',
+    fontWeight: FontWeight.w400,
+    height: 1.80,
+    letterSpacing: 0.05,
+  );
+  static const TextStyle textRegular12 = TextStyle(
+    color: Color(0xFF7D7E83),
+    fontSize: 12,
+    fontFamily: 'Mulish',
+    fontWeight: FontWeight.w400,
+    height: 1.67,
+    letterSpacing: 0.06,
+  );
+
+  static const TextStyle textSemiBold12 = TextStyle(
+    color: Color(0xFF685CF0),
+    fontSize: 12,
+    fontFamily: 'Mulish',
+    fontWeight: FontWeight.w600,
+    height: 1.67,
+    letterSpacing: 0.06,
+  );
+
+  static const TextStyle textBold14 = TextStyle(
+    color: Color(0xFF4C4E55),
+    fontSize: 14,
+    fontFamily: 'Mulish',
+    fontWeight: FontWeight.w700,
+    height: 1.57,
+    letterSpacing: 0.07,
+  );
+
+  static const TextStyle textBold12 = TextStyle(
+    color: Color(0xFF02050F),
+    fontSize: 12,
+    fontFamily: 'Mulish',
+    fontWeight: FontWeight.w700,
+    height: 1.67,
+    letterSpacing: 0.06,
+  );
+
+  static const TextStyle textBold24 = TextStyle(
+    color: Color(0xFF02050F),
+    fontSize: 24,
+    fontFamily: 'Mulish',
+    fontWeight: FontWeight.w700,
+    height: 1.33,
+    letterSpacing: 0.12,
+  );
+}
