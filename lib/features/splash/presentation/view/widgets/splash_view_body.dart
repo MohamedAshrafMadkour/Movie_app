f921b6d94a3d4288c4d3d29200b048106d7d4473
Mohamed Madkour <madkourmohamed88@gmail.com>
2025-07-12 20:56:27 +0300

    handle navigation in splash view body

--- lib/features/splash/presentation/view/widgets/splash_view_body.dart
+++ lib/features/splash/presentation/view/widgets/splash_view_body.dart
@@ -1,10 +1,14 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_svg/flutter_svg.dart';
 import 'package:movie_app/core/constants/keys.dart';
+import 'package:movie_app/core/services/auth_service.dart';
 import 'package:movie_app/core/services/pref_storage.dart';
 import 'package:movie_app/core/utils/assets.dart';
 import 'package:movie_app/core/utils/styles.dart';
+import 'package:movie_app/features/Auth/presentation/view/login_view.dart';
 import 'package:movie_app/features/on_boarding/presentation/view/onboarding_view.dart';
+import 'package:movie_app/features/profile/presentation/view/profile_view.dart';
+import 'package:movie_app/features/ready/presentation/view/ready_view.dart';
 import 'package:movie_app/generated/l10n.dart';
 
 class SplashViewBody extends StatefulWidget {
@@ -44,19 +48,19 @@ class _SplashViewBodyState extends State<SplashViewBody> {
   void executionNavigation() {
     bool isOnboarding = PrefStorage.getBool(AllKeys.setOnboarding);
     Future.delayed(const Duration(seconds: 3), () {
-      // if (isOnboarding) {
-      //   if (AuthService.isLogin()) {
-      //     if (PrefStorage.getBool(AllKeys.setProfile)) {
-      //       Navigator.pushNamed(context, ReadyView.routeName);
-      //     } else {
-      //       Navigator.pushNamed(context, ProfileView.routeName);
-      //     }
-      //   } else {
-      //     Navigator.pushNamed(context, LoginView.routeName);
-      //   }
-      // } else {
-      Navigator.pushNamed(context, OnboardingView.routeName);
-      // }
+      if (isOnboarding) {
+        if (AuthService.isLogin()) {
+          if (PrefStorage.getBool(AllKeys.setProfile)) {
+            Navigator.pushNamed(context, ReadyView.routeName);
+          } else {
+            Navigator.pushNamed(context, ProfileView.routeName);
+          }
+        } else {
+          Navigator.pushNamed(context, LoginView.routeName);
+        }
+      } else {
+        Navigator.pushNamed(context, OnboardingView.routeName);
+      }
     });
   }
 }
